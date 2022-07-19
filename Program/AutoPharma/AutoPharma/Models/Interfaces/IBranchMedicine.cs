b2675eee2c1557983d6488c101ce3a9a0fd002b4
Yousef Samara <yousef.aa.samara@gmail.com>
2022-07-20 00:19:21 +0300

    inter

--- Program/AutoPharma/AutoPharma/Models/Interfaces/IBranchMedicine.cs
+++ Program/AutoPharma/AutoPharma/Models/Interfaces/IBranchMedicine.cs
@@ -1,6 +1,17 @@
-﻿namespace AutoPharma.Models.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AutoPharma.Models.Interfaces
 {
     public interface IBranchMedicine
     {
+        Task<List<BranchMedicine>> GetAllBranchMedicine();
+        Task<BranchMedicine> GetBranchMedicine(int Id);
+        Task DeleteBranchMedicine(int Id);
+        Task<BranchMedicine> CreateBranchMedicine(BranchMedicine branchMedicine);
+        Task<BranchMedicine> UpdateBranchMedicine(int Id, BranchMedicine branchMedicine);
+
+        Task AddMedicineToBranch(int branchId, int medicineId);
+        Task RemoveMedicineFromBranch(int branchId, int medicineId);
     }
 }
