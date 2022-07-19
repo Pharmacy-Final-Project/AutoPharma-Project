f1d9f92f4c11e46547e132915cccaca2c3690581
Yousef Samara <yousef.aa.samara@gmail.com>
2022-07-20 00:37:17 +0300

    cont

--- Program/AutoPharma/AutoPharma/Startup.cs
+++ Program/AutoPharma/AutoPharma/Startup.cs
@@ -39,6 +39,7 @@ namespace AutoPharma
             services.AddTransient<IBranch, BranchService>();
             services.AddTransient<IMedicine, MedicineService>();
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
